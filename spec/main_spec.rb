b5e702905c54e3ebabb1e0d479b6de38cfbefe6a
zork <adampapirnik@gmail.com>
2022-03-28 13:09:02 -0500

    RSpec tests for place_piece()

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -12,10 +12,31 @@ describe Game do
       end
 
       it 'returns false when column is full' do
-        game = Game.new(Array.new(7) { Array.new(7, 'X') })
+        game = Game.new(Array.new(7) { Array.new(6, 'X') })
         chosen_column = 1
         result = game.valid_move?(chosen_column)
         expect(result).to be false
+      end
+    end
+  end
+
+  describe '#place_piece' do
+    subject(:game) { described_class.new }
+    context 'When placing piece on empty column' do
+      it 'Lands at the bottom of the column (position 6)' do
+        chosen_column = 3
+        game.place_piece(chosen_column, 'X')
+        bottom = game.instance_variable_get(:@board[chosen_column][6])
+        expect(bottom).to equal('X')
+      end
+    end
+
+    context 'When placing piece on column with 3 pieces already placed' do
+      it 'Lands two spaces from the top (position 2)' do
+        chosen_column = 3
+        game.place_piece(chosen_column, 'X')
+        middle = game.instance_variable_get(:@board[chosen_column][2])
+        expect(middle).to equal('X')
       end
     end
   end
