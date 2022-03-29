bba2120ce00483f533f4b9bbbd0bdde3946e0e0f
zork <adampapirnik@gmail.com>
2022-03-29 10:15:26 -0500

    Implemented place_piece()

--- lib/main.rb
+++ lib/main.rb
@@ -1,12 +1,23 @@
 #Game class
 class Game
-  def initialize( board = Array.new(7) { Array.new(7, ' ') })
+  def initialize(board = Array.new(7) { Array.new(6, ' ') })
     @board = board
   end
 
+  # Places player's piece in passed column
+  def place_piece(column, player)
+    @board[column].each_with_index do |space, index|
+      if space == ' '
+        @board[column][index] = player
+        break
+      end
+    end
+  end
+
+  # Checks if column is available to have a piece placed
   def valid_move?(column)
     return false if column.negative? || column > 6
-    return false if @board[column][0] != ' '
+    return false if @board[column][5] != ' '
 
     true
   end
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -23,20 +23,23 @@ describe Game do
   describe '#place_piece' do
     subject(:game) { described_class.new }
     context 'When placing piece on empty column' do
-      it 'Lands at the bottom of the column (position 6)' do
+      it 'Lands at the bottom of the column (position 0)' do
         chosen_column = 3
         game.place_piece(chosen_column, 'X')
-        bottom = game.instance_variable_get(:@board[chosen_column][6])
-        expect(bottom).to equal('X')
+        board = game.instance_variable_get(:@board)
+        bottom = board[chosen_column][0]
+        expect(bottom).to eq('X')
       end
     end
 
     context 'When placing piece on column with 3 pieces already placed' do
-      it 'Lands two spaces from the top (position 2)' do
+      it 'Lands two spaces from the top (position 3)' do
         chosen_column = 3
+        game = described_class.new(Array.new(7) { ['X', 'X', 'X', ' ', ' ', ' '] })
         game.place_piece(chosen_column, 'X')
-        middle = game.instance_variable_get(:@board[chosen_column][2])
-        expect(middle).to equal('X')
+        board = game.instance_variable_get(:@board)
+        middle = board[chosen_column][3]
+        expect(middle).to eq('X')
       end
     end
   end
